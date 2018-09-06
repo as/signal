46e30327a5fba998ca1a58d1a8ae4b2d7e4ee8c6
as <as.utf8@gmail.com>
2018-09-06 01:39:52 -0700

    add inverse test

--- ft_test.go
+++ ft_test.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"math"
+	"math/cmplx"
 	"reflect"
 	"testing"
 )
+
+func TestMath(t *testing.T) {
+	want := cmplx.Sqrt(-1) * 2 * math.Pi
+	if i2pi != want {
+		t.Fatalf("bad complex exponential: have %v, want %v", i2pi, want)
+	}
+	t.Logf("exp %v", cmplx.Exp(i2pi))
+	t.Logf("exp %v", cmplx.Exp(want))
+}
 
 func TestVectors(t *testing.T) {
 	for _, tc := range []struct {
@@ -11,10 +22,25 @@ func TestVectors(t *testing.T) {
 		o []complex128
 	}{
 		{i: []complex128{1, 0, 0, 0, 0, 0, 0, 0}, o: []complex128{.125, .125, .125, .125, .125, .125, .125, .125}},
-		{i: []complex128{0, 1, 0, 0, 0, 0, 0, 0}, o: []complex128{0.125, 0.088, 0, -0.088, -0.125, -0.088, 0, 0.088}},
+		{i: []complex128{0, 1, 0, 0, 0, 0, 0, 0}, o: []complex128{0.125, 0.088-0.088i, 0-0.125i, -0.088-0.088i, -0.125, -0.088+0.088i, 0+0.125i, 0.088+0.088i}},
 	} {
-		have := FT(tc.i...)
+		have := DFT(tc.i...)
 		if want := tc.o; !reflect.DeepEqual(have, want) {
+			t.Logf("bad output: have %v\n\twant %v", have, want)
+		}
+	}
+}
+
+
+func TestInvert(t *testing.T) {
+	for _, tc := range []struct {
+		i []complex128
+	}{
+		{i: []complex128{1, 0, 0, 0, 0, 0, 0, 0}},
+		{i: []complex128{0, 1, 0, 0, 0, 0, 0, 0}},
+	} {
+		have := IDFT(DFT(tc.i...)...)
+		if want := tc.i; !reflect.DeepEqual(have, want) {
 			t.Logf("bad output: have %v want %v", have, want)
 		}
 	}
